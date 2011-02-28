be384b9587d6358f990374f2b5cf51800265d1d6
Tomasz <tomekk_88@notowany.pl>
2011-02-28 19:09:45 +0100

    Szkielet aplikacji

---
+++ app/controllers/main_page_controller.rb
@@ -0,0 +1,6 @@
+class MainPageController < ApplicationController
+  def index
+	@title = "totalFPS"
+  end
+
+end
---
+++ public/javascripts/application.js
@@ -0,0 +1,6 @@
+// Place your application-specific JavaScript functions and classes here
+// This file is automatically included by javascript_include_tag :defaults
+$(document).ready(function(){
+	$('#flash_error').delay(4000).fadeOut(1000);
+	$('#flash_notice').delay(4000).fadeOut(1000);
+});
